98b183894fa8d8e0371cc62a345215aa3e7a2582
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2023-03-20 17:43:15 +0100

    updated functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,48 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello World!");
+const { getDatabase } = require("firebase-admin/database");
+
+admin.initializeApp();
+
+const express = require("express");
+const app = express();
+
+app.get("/screams", (req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then((data) => {
+      let screens = [];
+      data.forEach((doc) => {
+        screens.push(doc.data());
+      });
+
+      return res.json(screens);
+    })
+    .catch((err) => console.error(err));
 });
+
+exports.createScream = functions.https.onRequest((req, res) => {
+
+  const response = req.body
+
+  admin
+    .firestore()
+    .collection("screams")
+    .add({
+      body: response.body,
+      userHandle: response.userHandle,
+      createdAt: admin?.firestore?.Timestamp?.fromDate(new Date()),
+    })
+    .then((data) => {
+      res.json({ message: `document ${data.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json("Something went wrong");
+      console.error(err);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
